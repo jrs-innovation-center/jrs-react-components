28c3b45abf0965714cc7fedb9c49f407edd41a5e
Tom Wilson <tom@jackhq.com>
2017-07-09 08:46:34 -0400

    updated config

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,9 @@
 import buble from 'rollup-plugin-buble'
 import jsx from 'rollup-plugin-jsx'
-import resolve from 'rollup-plugin-node-resolve'
-import commonjs from 'rollup-plugin-commonjs'
 
 export default {
   entry: 'src/index.js',
-  plugins: [
-    buble(),
-    jsx({ factory: 'React.createElement' }),
-    resolve({
-      module: true,
-      jsnext: true,
-      main: true,
-      browser: true
-    }),
-    commonjs()
-  ],
+  plugins: [buble(), jsx({ factory: 'React.createElement' })],
   targets: [
     { dest: 'dist/bundle.cjs.js', format: 'cjs' },
     { dest: 'dist/bundle.umd.js', format: 'umd' },
