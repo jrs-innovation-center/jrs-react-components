21f90cd9a97b88a128fb15e6bb252df27995c3ea
Tom <tom@jackhq.com>
2017-07-18 13:53:51 -0400

    included ramda and proptypes

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,17 @@
 import buble from 'rollup-plugin-buble'
 import jsx from 'rollup-plugin-jsx'
+import resolve from 'rollup-plugin-node-resolve'
+import commonjs from 'rollup-plugin-commonjs'
 
 export default {
   entry: 'src/index.js',
-  external: ['prop-types', 'ramda', 'react'],
-  plugins: [buble(), jsx({ factory: 'React.createElement' })],
+  external: ['react'],
+  plugins: [
+    buble(),
+    jsx({ factory: 'React.createElement' }),
+    resolve(),
+    commonjs()
+  ],
   targets: [
     { dest: 'dist/bundle.cjs.js', format: 'cjs' },
     { dest: 'dist/bundle.umd.js', format: 'umd' },
